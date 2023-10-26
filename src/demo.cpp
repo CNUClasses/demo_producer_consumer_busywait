78495ac7b5a2c152bbeea3d199eeff272b92fe59
Keith Perkins <keith.perkins@cnu.edu>
2023-10-25 23:43:02 -0400

    made into bank accounts

--- src/demo.cpp
+++ src/demo.cpp
@@ -7,83 +7,30 @@
 #include <atomic>
 using namespace std;
 
-bool done=false;
-int global=0;
-mutex m;
+int bal=0;
 
-//1.fix the ugly couts
-void out1(string s){
-	lock_guard<mutex> lck(m);
-	cout<<s<<endl;
-}
-
-void produce(int id,int numb){
-	while(numb>0){
-		global++;
-		numb--;
-		out1("Thd "+to_string(id)+" produced, global="+to_string(global));
-//		cout<<"Thread "<<id<<"produced one"<<endl;
-		//snooze for a bit to simulate work
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+void deposit(int id,int numbdeposits, int amt){
+	while(numbdeposits>0){
+		bal=bal+amt;
+		numbdeposits--;
+		cout<<"Thread "<<id<<" deposited "<<amt<<" bal="<<bal<<endl;
 	}
-	out1("DONE!");
-	done=true;
 }
 
-void consume(int id){
-	while(!done || global>0){
-		if( global>0){
-			global--;
-			out1("   Thd "+to_string(id)+" consumed, global="+to_string(global));
-	//		cout<<" Thread "<<id<<"consumed one"<<endl;
-		}else
-			out1("WAITING");
+void withdraw(int id,int amt ){
+	while(bal>0){
+		bal--;
+		cout<<" Thread "<<id<<" withdrew "<<amt<<" bal="<<bal<<endl;
 	}
 }
 
 int main() {
-	thread t1(produce,1,100);
-//	thread t2(produce,2,100);
-	thread t3(consume,3);
-//	thread t4(consume,4);
+	thread t1(deposit,1,100,1);
+	thread t2(withdraw,2,1);
 
 	t1.join();
-//	t2.join();
-	t3.join();
-//	t4.join();
-	cout<<" Done! global="<<global<<endl;
+	t2.join();
+
+	cout<<"DONE! bal="<<bal<<endl;
 }
 
-
-//
-//const int TOTAL_THREADS		=20;
-//bool doWork=true;
-//int bal=0;
-//
-//void deposit(int id,int numbdeposits, int amt){
-//	while(numbdeposits>0){
-//		bal=bal+amt;
-//		numbdeposits--;
-//		cout<<"Thread "<<id<<"deposited"<<amt<<endl;
-//	}
-//}
-//
-//void withdraw(int id,int amt ){
-//	while(bal>0){
-//		bal--;
-//		cout<<" Thread "<<id<<"withdrew "<<amt<<endl;
-//	}
-//}
-//
-//int main() {
-//	thread t1(produce,1,100);
-//	thread t2(produce,2,100);
-//	thread t3(consume,3);
-//	thread t4(consume,4);
-//
-//	t1.join();
-//	t2.join();
-//	t3.join();
-//	t4.join();
-//}
-//
