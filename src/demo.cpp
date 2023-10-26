a5c99078608e65f124d12316b447d50aa39a888b
Keith Perkins <keith.perkins@cnu.edu>
2023-10-25 23:31:57 -0400

    solution

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
 #include <time.h>
@@ -6,55 +7,71 @@
 #include <atomic>
 using namespace std;
 
-const int TOTAL_THREADS		=20;
-bool doWork=true;
+bool done=false;
 int global=0;
+mutex m;
+
+//1.fix the ugly couts
+void out1(string s){
+	lock_guard<mutex> lck(m);
+	cout<<s<<endl;
+}
 
 void produce(int id,int numb){
 	while(numb>0){
 		global++;
 		numb--;
-		cout<<"Thread "<<id<<"produced one"<<endl;
+		out1("Thd "+to_string(id)+" produced, global="+to_string(global));
+//		cout<<"Thread "<<id<<"produced one"<<endl;
+		//snooze for a bit to simulate work
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
+	out1("DONE!");
+	done=true;
 }
 
 void consume(int id){
-	while(global>0){
-		global--;
-		cout<<" Thread "<<id<<"consumed one"<<endl;
+	while(!done || global>0){
+		if( global>0){
+			global--;
+			out1("   Thd "+to_string(id)+" consumed, global="+to_string(global));
+	//		cout<<" Thread "<<id<<"consumed one"<<endl;
+		}else
+			out1("WAITING");
 	}
 }
 
 int main() {
 	thread t1(produce,1,100);
-	thread t2(produce,2,100);
+//	thread t2(produce,2,100);
 	thread t3(consume,3);
-	thread t4(consume,4);
+//	thread t4(consume,4);
 
 	t1.join();
-	t2.join();
+//	t2.join();
 	t3.join();
-	t4.join();
+//	t4.join();
+	cout<<" Done! global="<<global<<endl;
 }
 
 
 //
 //const int TOTAL_THREADS		=20;
 //bool doWork=true;
-//int global=0;
+//int bal=0;
 //
-//void produce(int id,int numb){
-//	while(numb>0){
-//		global++;
-//		numb--;
-//		cout<<"Thread "<<id<<"produced one"<<endl;
+//void deposit(int id,int numbdeposits, int amt){
+//	while(numbdeposits>0){
+//		bal=bal+amt;
+//		numbdeposits--;
+//		cout<<"Thread "<<id<<"deposited"<<amt<<endl;
 //	}
 //}
 //
-//void consume(int id){
-//	while(global>0){
-//		global--;
-//		cout<<" Thread "<<id<<"consumed one"<<endl;
+//void withdraw(int id,int amt ){
+//	while(bal>0){
+//		bal--;
+//		cout<<" Thread "<<id<<"withdrew "<<amt<<endl;
 //	}
 //}
 //
